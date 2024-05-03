648d3e438fd5bc4c289e446c610761da5875b7fe
yzemaze <dev@yzemaze.de>
2024-05-03 21:19:04 +0200

    description moved to comment

--- starting-player-tag.user.js
+++ starting-player-tag.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         BGA starting player tag
-// @description  Denotes starting player with a little power icon (+ title). It performs fine for live games, but doesn’t work reliably in turn-based games, because sometimes early log messages are missing.
+// @description  Denotes starting player with a little power icon (+ title).
 // @match        https://*.boardgamearena.com/archive/replay/*
 // @match        https://*.boardgamearena.com/*/*table*
 // @icon         https://x.boardgamearena.net/data/themereleases/231110-1000/img/logo/logo.png
@@ -12,6 +12,13 @@
 // @author       yzemaze
 // @license      GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
 // ==/UserScript==
+
+// The script should work fine for replays and live games. Due to early gamelog
+// entries being wiped in specific cases, it doesn’t function reliably in
+// turn-based games, as spectator or when refreshing your game. I couldn’t
+// figure out when exactly this happens. It seems to be a restriction applied
+// after a specific number of moves or elapsed time. Please beware of this
+// issue and use the script with caution.
 
 "use strict";
 function getStartPlayerNameFromLog() {
